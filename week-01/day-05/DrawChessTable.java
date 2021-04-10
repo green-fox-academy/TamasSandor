f0548abdb21fcff1eaff9834aa8cff31761e4d7d
Sandor Tamas <thomas.sandor@gmail.com>
2021-04-10 16:26:19 +0200

    add(provide a less obvious solution for the DrawChessTable problem)

--- week-01/day-05/DrawChessTable.java
+++ week-01/day-05/DrawChessTable.java
@@ -15,13 +15,25 @@ public class DrawChessTable {
         //
 
         // Because a chess board can ONLY and ONLY be 8 by 8 there is a quite obvious answer:
-        System.out.println("% % % %");
-        System.out.println(" % % % %");
-        System.out.println("% % % %");
-        System.out.println(" % % % %");
-        System.out.println("% % % %");
-        System.out.println(" % % % %");
-        System.out.println("% % % %");
-        System.out.println(" % % % %");
+        //System.out.println("% % % %");
+        //System.out.println(" % % % %");
+        //System.out.println("% % % %");
+        //System.out.println(" % % % %");
+        //System.out.println("% % % %");
+        //System.out.println(" % % % %");
+        //System.out.println("% % % %");
+        //System.out.println(" % % % %");
+
+        // A less obvious answer apparently also exists
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 != 0) {
+                    System.out.print(j % 2 != 0 ? "%" : " ");
+                } else {
+                    System.out.print(j % 2 == 0 ? "%" : " ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
