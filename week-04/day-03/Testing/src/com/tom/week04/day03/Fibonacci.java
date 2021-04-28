e937fc640f13eae70071cc4c9d1d2f746e7dcfe7
Sandor Tamas <thomas.sandor@gmail.com>
2021-04-28 16:57:03 +0200

    fix(Add test case for negative numbers in FibonacciTest)

--- week-04/day-03/Testing/src/com/tom/week04/day03/Fibonacci.java
+++ week-04/day-03/Testing/src/com/tom/week04/day03/Fibonacci.java
@@ -2,7 +2,9 @@ package com.tom.week04.day03;
 
 public class Fibonacci {
   public int fibonacci(int n) {
-    if (n <= 1) {
+    if (n < 0) {
+      throw new IllegalArgumentException();
+    } else if (n <= 1) {
       return n;
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
--- week-04/day-03/Testing/test/com/tom/week04/day03/FibonacciTest.java
+++ week-04/day-03/Testing/test/com/tom/week04/day03/FibonacciTest.java
@@ -1,6 +1,7 @@
 package com.tom.week04.day03;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,5 +22,10 @@ class FibonacciTest {
   @Test
   void testTenthIndex() {
     assertEquals(55, fibonacci.fibonacci(10));
+  }
+
+  @Test
+  void testNegativeIndex() {
+    assertThrows(IllegalArgumentException.class, () -> fibonacci.fibonacci(-1));
   }
 }
